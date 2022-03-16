07e04a69b8283f601dbbbcd165e7c08f50ed1347
venomcodsyoutube <101553606+venomcodsyoutube@users.noreply.github.com>
2022-03-16 20:59:23 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,15 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { VStack } from '@chakra-ui/react'
 import Header from './components/Header'
+import Rec from './components/RecCourses'
 
 export default function Home() {
   return (
     <VStack>
     <Head><title>VenomCodes | Home</title></Head>
         <Header />
+        <Rec />
     </VStack>
   )
 }
