e5b6c00ca14d4e467dbe0f63544d3d1b307d76b0
venomcodsyoutube <101553606+venomcodsyoutube@users.noreply.github.com>
2022-03-19 17:47:16 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,14 @@
 import '../styles/globals.css'
 import Navbar from './components/Navbar'
 import {ChakraProvider, ColorModeScript} from '@chakra-ui/react'
-// import Menu from './components/Menu'
+import Menu from './components/menu'
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
       <ColorModeScript initialColorMode='light'></ColorModeScript>
       <Navbar />
-      {/* <Menu /> */}
+       <Menu />
       <Component {...pageProps} />
     </ChakraProvider>
   )
