85e0c4470f3eb9b7eb0a6c14e08e98625e463e98
venomcodsyoutube <101553606+venomcodsyoutube@users.noreply.github.com>
2022-03-31 18:44:28 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,14 @@ import '../styles/globals.css'
 import Navbar from './components/Navbar'
 import {ChakraProvider, ColorModeScript} from '@chakra-ui/react'
 import Menu from './components/menu'
+import Head from "next/head"
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
+      <Head>
+    <lnk rel="./components/CHANNEL LOGO.png" type="icon" />
+    </Head>
       <ColorModeScript initialColorMode='light'></ColorModeScript>
       <Navbar />
        <Menu />
