1e67ac4c2b71f0e63b44236a39db5e5ec12f57c2
venomcodsyoutube <101553606+venomcodsyoutube@users.noreply.github.com>
2022-03-31 18:52:57 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
       <Head>
-    <link href="./components/CHANNEL LOGO.png" type="icon" />
+    <link href="https://yt3.ggpht.com/WW44k9Wpr5W8IzAKgiunKsp-Ue_tnhdIdOw3xnmiGoALEdrx9PXk_AdNznuGPjBIWF9hTvdm=s176-c-k-c0x00ffffff-no-rj" type="icon" />
     </Head>
       <ColorModeScript initialColorMode='light'></ColorModeScript>
       <Navbar />
